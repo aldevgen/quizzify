44e8fd0c07713432d948b72194c523e0504b9f2a
Alanna DEVLIN GENIN <alannadevlingenin@gmail.com>
2024-12-24 00:51:31 +0100

    feat: define top service and routers methods

--- api/quizzify/routers/artists/router.py
+++ api/quizzify/routers/artists/router.py
@@ -10,41 +10,24 @@ from quizzify.utils.schemas import TimeRange
 router = APIRouter()
 
 
-@router.post(
+@router.get(
     path="/top",
-    response_model=List[Dict],
     summary="Return the user's top artists from Spotify",
     description=(
-        "Return the user's top artists from Spotify. This endpoint requires an "
-        "access token."
+        "Return the user's top artists from Spotify. "
+        "This endpoint requires an access token."
     ),
 )
-async def get_top_artists(
-    time_range: TimeRange,
-    limit: int,
-):
+async def get_top_artists():
     """Return the user's top artists from Spotify.
-
-    Parameters
-    ----------
-    time_range : str, optional
-        The time range for the top artists. Valid values are 'short_term',
-        'medium_term' and 'long_term'.
-    limit : int
-        The number of artists to fetch (the maximum is set to 50 by the Spotify API).
 
     Returns
     -------
     list
         A list of the user's top artists.
     """
-    if limit > 50:
-        raise ValueError("Limit cannot exceed 50 artists.")
-
-    top_artists = service.get_top_artists(
-        time_range=time_range,
-        limit=limit,
-    )
+    user_id = spotify_get_user_id()
+    top_artists = service.get_top_artists(user_id=user_id)
     return top_artists
 
 
@@ -64,3 +47,41 @@ async def get_random_artist():
     user_id = spotify_get_user_id()
     random_artist = service.get_random_artist(user_id=user_id)
     return random_artist
+
+
+@router.post(
+    path="/top",
+    response_model=List[Dict],
+    summary="Return the user's top artists from Spotify",
+    description=(
+        "Return the user's top artists from Spotify. This endpoint requires an "
+        "access token."
+    ),
+)
+async def post_top_artists(
+    time_range: TimeRange,
+    limit: int = 10,
+):
+    """Return the user's top artists from Spotify.
+
+    Parameters
+    ----------
+    time_range : str, optional
+        The time range for the top artists. Valid values are 'short_term',
+        'medium_term' and 'long_term'.
+    limit : int
+        The number of artists to fetch (the maximum is set to 50 by the Spotify API).
+
+    Returns
+    -------
+    list
+        A list of the user's top artists.
+    """
+    if limit > 50:
+        raise ValueError("Limit cannot exceed 50 artists.")
+
+    top_artists = service.insert_top_artists(
+        time_range=time_range,
+        limit=limit,
+    )
+    return top_artists
